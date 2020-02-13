9af1d2d262c521ac97a10624dff6fc60a9c1f29b
rabbai666 <rabbai666@gmail.com>
2020-02-13 17:17:43 +0100

    initial commit

---
+++ tobikanayo/tobi-kanayo.js
@@ -0,0 +1,39 @@
+// const calculator = document.querySelector('.housecalc')
+// const keys = calculator.querySelector('.calculator_keys')
+
+// keys.addEventListener('click', e => {
+//  if (e.target.matches('input')) {
+//    // Do something
+
+//  }
+ 
+// })
+
+// const key = e.target
+// const action = key.dataset.action
+// console.log("input");
+
+// if (!action) {
+//     console.log('number key!')
+
+
+//   }
+
+//   if (
+//     action === 'subtract' ||
+//     action === 'multiply' ||
+//     action === 'divide'
+//   ) {
+//     console.log('operator key!')
+//   }
+
+
+function myFunction(button) {
+    var x = button.value;
+    document.getElementById("demo").innerHTML = x;
+}
+
+
+
+
+
